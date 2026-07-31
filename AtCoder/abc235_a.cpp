21c8d287a2ec3bfa3054e2ea73a72d210314e7f9
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-31 12:26:20 +0600

    [Sync] abc235_a on AtCoder 🚀

---
+++ AtCoder/abc235_a.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc&#43;&#43;.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    cout << 111*(s[0]-&#39;0&#39;&#43;s[1]-&#39;0&#39;&#43;s[2]-&#39;0&#39;) << endl;
+    return 0;
+}
