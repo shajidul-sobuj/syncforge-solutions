56017e498c530b9a72544f0ad7de228aa4a29909
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-31 12:27:12 +0600

    [Sync] abc289_a on AtCoder 🚀

---
+++ AtCoder/Contest_ABC289/abc289_a.cpp
@@ -0,0 +1,25 @@
+/*
+ * AtCoder Auto Push - Synced Solution
+ * 
+ * Problem: abc289_a
+ * Contest: ABC289
+ * Link: https://atcoder.jp/contests/abc289/tasks/abc289_a
+ * Submission ID: 72629792
+ * Language: C++23 (GCC 15.2.0)
+ * Execution Time: 1 ms
+ * Submitted At: Wed, 21 Jan 2026 14:03:02 GMT
+ */
+
+#include <bits/stdc&#43;&#43;.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    for (char c : s)
+    {
+        if (c==&#39;0&#39;) cout << &#34;1&#34;;
+        else cout << &#34;0&#34;;
+    }
+    return 0;
+}
