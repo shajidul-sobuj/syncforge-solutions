36d78cbd0f8c82120077b0651787d5f969e4252e
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-31 11:57:38 +0600

    [Sync] Team on Codeforces 🚀

--- Codeforces/Team.cpp
+++ Codeforces/Team.cpp
@@ -1,1 +1,30 @@
-#include <bits/stdc++.h>using namespace std;#define ll long long#define endl '\n'// cont int N = 1e5;   void boom(){     int n; cin >> n;    int sum = 0;    for (int i=0;i<n;i++){        int x,y,z; cin >> x >> y >> z;        int t = (x+y+z);        if(t>=2) sum++;    }    cout << sum << endl;      }     int main() {    ios_base::sync_with_stdio(false);    cin.tie(nullptr);     int t = 1;    // cin >> t;    while(t--)boom();    return 0;}
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl &#39;\n&#39;
+
+void boom()
+{
+    int n; cin >> n;
+    vector<vector<int>> a(n, vector<int>(3,0));
+    int ans = 0;
+    for (int i=0;i<n;i++){
+        cin >> a[i][0] >> a[i][1] >> a[i][2];
+
+        int sum = (a[i][0]+a[i][1]+a[i][2]);
+        if (sum >= 2) ans++;
+    }
+    cout << ans << endl;
+
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+    while(t--) boom();
+    return 0;
+}
