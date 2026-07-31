5e65c24ef64a8ee7ef8b7b2f032d88adeb8bc1a9
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-31 12:24:26 +0600

    [Sync] Team on Codeforces 🚀

--- Codeforces/Codeforces_Round_143_(Div._2)/A_Team.cpp
+++ Codeforces/Codeforces_Round_143_(Div._2)/A_Team.cpp
@@ -11,38 +11,4 @@
  * Submitted At: Sun, 19 Jul 2026 10:04:32 GMT
  */
 
-#include <bits/stdc++.h>
-using namespace std;
-#define ll long long
-#define endl &#39;\n&#39;
-// cont int N = 1e5;
-
-
-
-void boom(){
-
-    int n; cin >> n;
-    int sum = 0;
-    for (int i=0;i<n;i++){
-        int x,y,z; cin >> x >> y >> z;
-        int t = (x+y+z);
-        if(t>=2) sum++;
-    }
-    cout << sum << endl;
-
-     
-}
-
-
-
-
-
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int t = 1;
-    // cin >> t;
-    while(t--)boom();
-    return 0;
-}
+#include <bits/stdc++.h>using namespace std;#define ll long long#define endl '\n'// cont int N = 1e5;   void boom(){     int n; cin >> n;    int sum = 0;    for (int i=0;i<n;i++){        int x,y,z; cin >> x >> y >> z;        int t = (x+y+z);        if(t>=2) sum++;    }    cout << sum << endl;      }     int main() {    ios_base::sync_with_stdio(false);    cin.tie(nullptr);     int t = 1;    // cin >> t;    while(t--)boom();    return 0;}
