36ba82ac6a81b9cf8650ddb11d2a791b6b7eda14
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-31 12:02:40 +0600

    [Sync] Watermelon on Codeforces 🚀

---
+++ Codeforces/Codeforces_Beta_Round_4_(Div._2_Only)/A_Watermelon.cpp
@@ -0,0 +1,23 @@
+/*
+ * Codeforces Auto Push - Synced Solution
+ * 
+ * Problem: 4A - Watermelon
+ * Link: https://codeforces.com/contest/4/problem/A
+ * Submission ID: 382843143
+ * Language: C++23 (GCC 14-64, msys2)
+ * Execution Time: 62 ms | Memory: N/A
+ * Verdict: OK
+ * Rating: 800 | Tags: brute force, math
+ * Submitted At: Wed, 15 Jul 2026 19:16:12 GMT
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x;
+    cin >> x;
+    if (x>2 && x%2==0) cout << "YES\n";
+    else cout << "NO\n";
+    return 0;
+}
