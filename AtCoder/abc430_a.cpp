fa957af427ba9adb3926d9a8d7a02fcd558862ec
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-31 12:24:49 +0600

    [Sync] abc430_a on AtCoder 🚀

---
+++ AtCoder/abc430_a.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c,d;
+    scanf(&#34;%d %d %d %d&#34;, &a, &b, &c, &d);
+    if (c>=a && d<b){
+        printf(&#34;Yes\n&#34;);
+    }else {
+        printf(&#34;No\n&#34;);
+    }
+    return 0;
+}
+
+
+
