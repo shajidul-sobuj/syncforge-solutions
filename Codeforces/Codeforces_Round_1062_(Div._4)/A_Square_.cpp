6ef8ee4943621d2dd167e45d39ac2f220241d814
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-31 12:29:06 +0600

    [Sync] Square_ on Codeforces 🚀

--- Codeforces/Codeforces_Round_1062_(Div._4)/A_Square_.cpp
+++ Codeforces/Codeforces_Round_1062_(Div._4)/A_Square_.cpp
@@ -11,35 +11,4 @@
  * Submitted At: Sat, 11 Jul 2026 17:28:34 GMT
  */
 
-#include <bits/stdc++.h>
-using namespace std;
-
-#define ll long long
-#define ull unsigned long long
-#define all(x) (x).begin(), (x).end()
-#define endl &#39;\n&#39;
-const int N = 1e5+10;
-
-void boom()
-{
-    int a,b,c,d; cin >> a >> b >> c >> d;
-    if (a == b && b == c && c == d) cout << "YES\n";
-    else cout << "NO\n";
-
-}
-
-
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int t = 1;
-    cin >> t;
-    while (t--)
-    {
-        boom();
-    }
-
-    return 0;
-}
+#include <bits/stdc++.h>using namespace std; #define ll long long#define ull unsigned long long#define all(x) (x).begin(), (x).end()#define endl '\n'const int N = 1e5+10; void boom(){    int a,b,c,d; cin >> a >> b >> c >> d;    if (a == b && b == c && c == d) cout << "YES\n";    else cout << "NO\n"; }  int main(){    ios::sync_with_stdio(false);    cin.tie(nullptr);     int t = 1;    cin >> t;    while (t--)    {        boom();    }     return 0;}
