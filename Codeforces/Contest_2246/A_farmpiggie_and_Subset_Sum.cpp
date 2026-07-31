595ca8491c81ccbaaa864e0b13338ef81d03a02e
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-31 11:59:43 +0600

    [Sync] farmpiggie_and_Subset_Sum on Codeforces 🚀

---
+++ Codeforces/Contest_2246/A_farmpiggie_and_Subset_Sum.cpp
@@ -0,0 +1,45 @@
+/*
+ * Codeforces Auto Push - Synced Solution
+ * 
+ * Problem: 2246A - farmpiggie_and_Subset_Sum
+ * Link: https://codeforces.com/contest/2246/problem/A
+ * Submission ID: 382244384
+ * Language: C++23 (GCC 14-64, msys2)
+ * Execution Time: 15 ms | Memory: N/A
+ * Verdict: OK
+ * Rating: N/A | Tags: constructive algorithms
+ * Submitted At: Sun, 12 Jul 2026 14:47:50 GMT
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define all(x) (x).begin(), (x).end()
+#define endl &#39;\n&#39;
+const int N = 1e5+10;
+
+void boom()
+{
+    int n; cin >> n;
+    for (int i=2;i<=n;i++) cout << i << " ";
+    cout << 1 << endl;
+
+}
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        boom();
+    }
+
+    return 0;
+}
