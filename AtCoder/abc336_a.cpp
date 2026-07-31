1587c73b5e96458ab2d3782fc0b3ec3f6b9f1e6c
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-31 12:26:06 +0600

    [Sync] abc336_a on AtCoder 🚀

---
+++ AtCoder/abc336_a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc&#43;&#43;.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    cout << &#34;L&#34;;
+    while(n--){
+        cout << &#34;o&#34;;
+    }
+    cout << &#34;ng\n&#34;;
+    return 0;
+}
