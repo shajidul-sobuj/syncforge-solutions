8da7c906c6092fa60c3448dd02d79f7ad75d0ec5
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-31 12:29:30 +0600

    [Sync] Iskander_and_Drawings on Codeforces 🚀

--- Codeforces/Codeforces_Round_1109_(Div._3)/A_Iskander_and_Drawings.cpp
+++ Codeforces/Codeforces_Round_1109_(Div._3)/A_Iskander_and_Drawings.cpp
@@ -11,46 +11,4 @@
  * Submitted At: Tue, 14 Jul 2026 14:44:08 GMT
  */
 
-#include <bits/stdc++.h>
-using namespace std;
-
-#define ll long long
-#define ull unsigned long long
-#define all(x) (x).begin(), (x).end()
-#define endl &#39;\n&#39;
-const int N = 1e5+10;
-
-void boom()
-{
-    int n; cin >> n;
-    string s; cin >> s;
-    int cnt = 0;
-    int ans = 0;
-    for (int i=0;i<n;i++){
-        if(s[i] == &#39;*&#39;){
-            ans = max(cnt, ans);
-            cnt = 0;
-        }else cnt++;
-    }
-    ans = max(cnt, ans);
-    ans++;
-    cout << ans/2 << endl;
-    // cout << ans;
-
-}
-
-
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int t = 1;
-    cin >> t;
-    while (t--)
-    {
-        boom();
-    }
-
-    return 0;
-}
+#include <bits/stdc++.h>using namespace std; #define ll long long#define ull unsigned long long#define all(x) (x).begin(), (x).end()#define endl '\n'const int N = 1e5+10; void boom(){    int n; cin >> n;    string s; cin >> s;    int cnt = 0;    int ans = 0;    for (int i=0;i<n;i++){        if(s[i] == '*'){            ans = max(cnt, ans);            cnt = 0;        }else cnt++;    }    ans = max(cnt, ans);    ans++;    cout << ans/2 << endl;    // cout << ans; }  int main(){    ios::sync_with_stdio(false);    cin.tie(nullptr);     int t = 1;    cin >> t;    while (t--)    {        boom();    }     return 0;}
