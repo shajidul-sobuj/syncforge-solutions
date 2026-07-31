3d7b975502b5087c22328bb2013672931b70fd68
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-07-31 12:00:18 +0600

    [Sync] Square_ on Codeforces 🚀

---
+++ Codeforces/Contest_2167/A_Square_.cpp
@@ -0,0 +1,45 @@
+/*
+ * Codeforces Auto Push - Synced Solution
+ * 
+ * Problem: 2167A - Square_
+ * Link: https://codeforces.com/contest/2167/problem/A
+ * Submission ID: 382141569
+ * Language: C++23 (GCC 14-64, msys2)
+ * Execution Time: 46 ms | Memory: N/A
+ * Verdict: OK
+ * Rating: 800 | Tags: math, sortings
+ * Submitted At: Sat, 11 Jul 2026 17:28:34 GMT
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define all(x) (x).begin(), (x).end()
+#define endl &#39;\n&#39;
+const int N = 1e5+10;
+
+void boom()
+{
+    int a,b,c,d; cin >> a >> b >> c >> d;
+    if (a == b && b == c && c == d) cout << "YES\n";
+    else cout << "NO\n";
+
+}
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        boom();
+    }
+
+    return 0;
+}
