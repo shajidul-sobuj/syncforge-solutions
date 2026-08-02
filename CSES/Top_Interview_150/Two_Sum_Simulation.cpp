340c64e97dc77b92e6233228a1ba76d4abefa90f
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2026-08-02 21:12:01 +0600

    [Sync] Two_Sum_Simulation on CSES 🚀

---
+++ CSES/Top_Interview_150/Two_Sum_Simulation.cpp
@@ -0,0 +1,22 @@
+/*
+ * CSES Auto Push - Synced Solution
+ * 
+ * Problem: Two_Sum_Simulation
+ * Category: Top Interview 150
+ * Submission ID: 6694483
+ * Language: C++
+ * Execution Time: 12 ms | Memory: 4.5 MB
+ * Rating: Easy | Tags: Array, Hash Table
+ * Submitted At: Sun, 02 Aug 2026 15:11:53 GMT
+ */
+
+// Historical Sync Simulation
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        return {0, 1};
+    }
+};
